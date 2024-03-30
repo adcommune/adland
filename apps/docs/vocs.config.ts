4c8e8d2b57cc5e1b8f94c35c078869c6aff0f2dd
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-03-30 09:15:25 +0100

    update docs theme

--- apps/docs/vocs.config.ts
+++ apps/docs/vocs.config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from "vocs";
 
 export default defineConfig({
   title: "AdLand",
+  font: {
+    google: "Montserrat",
+  },
+  logoUrl: "https://i.imgur.com/JUyaIUO.png",
+  theme: {
+    accentColor: {
+      backgroundAccent: { light: "#00429d", dark: "#77ae74" },
+      backgroundAccentHover: { light: "#00429d", dark: "#77ae74" },
+      backgroundAccentText: { light: "#f5f5f5", dark: "#f5f5f5" },
+      textAccent: { light: "#00429d", dark: "#ffb165" },
+      textAccentHover: { light: "#00429d", dark: "#ffb165" },
+      borderAccent: { light: "#00429d", dark: "#77ae74" },
+    },
+  },
   sidebar: [
     {
       text: "What is Adland ?",
