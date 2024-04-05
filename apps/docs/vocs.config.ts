0dc7bca5c73b64f93f49edf91237d8931f1dbc09
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-05 15:17:28 +0200

    docs: add basic webads example use-case doc page

--- apps/docs/vocs.config.ts
+++ apps/docs/vocs.config.ts
@@ -16,24 +16,34 @@ export default defineConfig({
       borderAccent: { light: "#00429d", dark: "#77ae74" },
     },
   },
+  socials: [
+    {
+      icon: "warpcast",
+      link: "https://warpcast.com/~/channel/adland",
+    },
+    {
+      icon: "github",
+      link: "https://github.com/nezz0746/adland",
+    },
+  ],
   sidebar: [
     {
       text: "What is Adland ?",
       link: "/what-is-adland",
     },
-    // {
-    //   text: "Use cases",
-    //   collapsed: true,
-    //   items: [
-    //     {
-    //       text: "Offchain: Display Ads",
-    //       link: "/use-cases/web-ads",
-    //     },
-    //     {
-    //       text: "Offchain: Social Media Ads",
-    //       link: "/use-cases/social-media-ads",
-    //     },
-    //   ],
-    // },
+    {
+      text: "Use cases",
+      collapsed: true,
+      items: [
+        {
+          text: "Web Ads",
+          link: "/use-cases/web-ads",
+        },
+        {
+          text: "Social Media Ads",
+          link: "/use-cases/social-media-ads",
+        },
+      ],
+    },
   ],
 });
