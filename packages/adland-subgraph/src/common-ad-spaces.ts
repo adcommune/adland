cfdb30c373e008dd398808598380c3dec7dd78ee
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-09 14:19:21 +0200

    feat: add AdLand subgraph package

--- packages/adland-subgraph/src/common-ad-spaces.ts
+++ packages/adland-subgraph/src/common-ad-spaces.ts
@@ -11,8 +11,8 @@ import {
   OwnershipTransferred as OwnershipTransferredEvent,
   TokenXSet as TokenXSetEvent,
   Transfer as TransferEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/CommonAdSpaces/CommonAdSpaces"
+  Upgraded as UpgradedEvent,
+} from "../generated/CommonAdSpaces/CommonAdSpaces";
 import {
   AdGroupCreated,
   AdSpaceCreated,
@@ -26,38 +26,35 @@ import {
   OwnershipTransferred,
   TokenXSet,
   Transfer,
-  Upgraded
-} from "../generated/schema"
+  Upgraded,
+} from "../generated/schema";
 
 export function handleAdGroupCreated(event: AdGroupCreatedEvent): void {
   let entity = new AdGroupCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.groupId = event.params.groupId
-  entity.beneficiary = event.params.beneficiary
+  );
+  entity.groupId = event.params.groupId;
+  entity.beneficiary = event.params.beneficiary;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleAdSpaceCreated(event: AdSpaceCreatedEvent): void {
   let entity = new AdSpaceCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.groupId = event.params.groupId
-  entity.adId = event.params.adId
-  entity.adSpace_adGroupId = event.params.adSpace.adGroupId
-  entity.adSpace_uri = event.params.adSpace.uri
-  entity.adSpace_strategy = event.params.adSpace.strategy
+  );
+  entity.groupId = event.params.groupId;
+  entity.adId = event.params.adId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleAdSpaceStrategyUpdated(
@@ -65,99 +62,99 @@ export function handleAdSpaceStrategyUpdated(
 ): void {
   let entity = new AdSpaceStrategyUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.adId = event.params.adId
-  entity.strategy = event.params.strategy
+  );
+  entity.adId = event.params.adId;
+  entity.strategy = event.params.strategy;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleAdSpaceURIUpdated(event: AdSpaceURIUpdatedEvent): void {
   let entity = new AdSpaceURIUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.adId = event.params.adId
-  entity.uri = event.params.uri
+  );
+  entity.adId = event.params.adId;
+  entity.uri = event.params.uri;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleAdminChanged(event: AdminChangedEvent): void {
   let entity = new AdminChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+  );
+  entity.previousAdmin = event.params.previousAdmin;
+  entity.newAdmin = event.params.newAdmin;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
   let entity = new BeaconUpgraded(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
+  );
+  entity.beacon = event.params.beacon;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleInitialized(event: InitializedEvent): void {
   let entity = new Initialized(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
+  );
+  entity.version = event.params.version;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -165,55 +162,55 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokenXSet(event: TokenXSetEvent): void {
   let entity = new TokenXSet(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.underlyingToken = event.params.underlyingToken
-  entity.superToken = event.params.superToken
+  );
+  entity.underlyingToken = event.params.underlyingToken;
+  entity.superToken = event.params.superToken;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleUpgraded(event: UpgradedEvent): void {
   let entity = new Upgraded(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
+  );
+  entity.implementation = event.params.implementation;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
