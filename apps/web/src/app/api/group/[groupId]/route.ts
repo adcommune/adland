2191f5973dd21b4e8c00f994f95fce0f59313fcd
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-10 21:41:13 +0200

    display group ad space & image if present

--- apps/web/src/app/api/group/[groupId]/route.ts
+++ apps/web/src/app/api/group/[groupId]/route.ts
@@ -1,30 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server'
-import { adland } from '@/lib/services'
-import { resolveAdSpaceWithMetadata } from '@/lib/helpers'
-import { AdSpaceQuery } from '@adland/webkit'
+import { AdLand } from '@/lib/services'
 
 type GetAdsRouteParams = { params: { groupId: string } }
 
 export async function GET(_: NextRequest, { params }: GetAdsRouteParams) {
-  const group = await adland
-    .adGroup({
-      id: params.groupId,
-    })
-    .then((response) => {
-      return response.adGroup
-    })
-
-  if (!group) {
-    throw new Error('Group not found')
-  } else {
-    const group_response = {
-      ...group,
-      adSpaces: await Promise.all(
-        group.adSpaces.map(async (adSpace) =>
-          resolveAdSpaceWithMetadata(adSpace as AdSpaceQuery['adSpace']),
-        ),
-      ),
-    }
-    return NextResponse.json(group_response)
-  }
+  return NextResponse.json(await new AdLand().getGroup(params.groupId))
 }
--- apps/web/src/app/app/group/[id]/page.tsx
+++ apps/web/src/app/app/group/[id]/page.tsx
@@ -1,25 +1,45 @@
 import { Container } from '@/components/Container'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { adland } from '@/lib/services'
+import { AdLand } from '@/lib/services'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const GroupPage = async ({ params: { id } }: { params: { id: string } }) => {
-  const { adGroup } = await adland.adGroup({ id })
+  const { adSpaces } = await new AdLand().getGroup(id)
 
   return (
     <Container className="p-4">
-      <div className="grid grid-cols-3 gap-2">
-        {adGroup?.adSpaces?.map((adSpace, index) => {
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
+        {adSpaces?.map((adSpace, index) => {
           return (
             <Link
               key={adSpace?.transactionHash + adSpace?.id}
               href={'/app/group/' + id + '/' + adSpace?.id}
             >
-              <Card className="">
+              <Card className="flex h-[400px] flex-col overflow-hidden">
                 <CardHeader>
-                  <CardTitle>AdSpace #{index}</CardTitle>
+                  <CardTitle className="font-body font-normal text-gray-500">
+                    AdSpace #{index}
+                  </CardTitle>
                 </CardHeader>
-                <CardContent>{/* <Image src={adSpace?.} /> */}</CardContent>
+                <CardContent className="relative flex w-full flex-grow flex-col items-center justify-center gap-2 bg-gray-100 py-4">
+                  {adSpace?.metadata?.description && (
+                    <p className="text-left text-sm">
+                      {adSpace?.metadata?.description}
+                    </p>
+                  )}
+                  {adSpace?.metadata?.imageGatewayURI && (
+                    <div className="flex w-full flex-grow bg-gray-400">
+                      <Image
+                        width={500}
+                        height={500}
+                        alt="AdSpace Image"
+                        className="h-full w-full object-contain"
+                        src={adSpace?.metadata?.imageGatewayURI}
+                      />
+                    </div>
+                  )}
+                </CardContent>
               </Card>
             </Link>
           )
