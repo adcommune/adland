a720817156ffb0869967a4cfc1f0583489729a23
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-12 23:32:49 +0200

    fix pinata gateway remote pattern

--- packages/adland-common/src/constants.testnet.ts
+++ packages/adland-common/src/constants.testnet.ts
@@ -4,6 +4,6 @@ import { Constants } from "./types";
 export const constants: Constants = {
   subgraphUrl:
     "https://api.thegraph.com/subgraphs/name/nezz0746/adland-optsepolia",
-  pinataPublicGateway: "amethyst-representative-mandrill-369.mypinata.cloud	",
+  pinataPublicGateway: "amethyst-representative-mandrill-369.mypinata.cloud",
   chain: optimismSepolia,
 };
