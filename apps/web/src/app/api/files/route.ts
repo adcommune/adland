efc3ebfa2c86836070c8ae7972b5de3140951917
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-13 08:00:51 +0200

    add log to file endpoint

--- apps/web/src/app/api/files/route.ts
+++ apps/web/src/app/api/files/route.ts
@@ -1,49 +1,51 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from 'next/server'
 
 export async function POST(request: NextRequest) {
   try {
-    const data = await request.formData();
-    const file: File | null = data.get("file") as unknown as File;
-    data.append("file", file);
-    data.append("pinataMetadata", JSON.stringify({ name: "File to upload" }));
-    const res = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
-      method: "POST",
+    const data = await request.formData()
+    const file: File | null = data.get('file') as unknown as File
+    data.append('file', file)
+    data.append('pinataMetadata', JSON.stringify({ name: 'File to upload' }))
+    const res = await fetch('https://api.pinata.cloud/pinning/pinFileToIPFS', {
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${process.env.PINATA_JWT}`,
       },
       body: data,
-    });
-    const { IpfsHash } = await res.json();
+    })
+    const { IpfsHash } = await res.json()
 
-    return NextResponse.json({ IpfsHash }, { status: 200 });
+    console.log(IpfsHash)
+
+    return NextResponse.json({ IpfsHash }, { status: 200 })
   } catch (e) {
-    console.log(e);
+    console.log(e)
     return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    )
   }
 }
 
 export async function GET() {
   try {
     const res = await fetch(
-      "https://api.pinata.cloud/data/pinList?status=pinned",
+      'https://api.pinata.cloud/data/pinList?status=pinned',
       {
-        method: "GET",
+        method: 'GET',
         headers: {
           Authorization: `Bearer ${process.env.PINATA_JWT}`,
         },
-      }
-    );
-    const resData = await res.json();
-    const files = resData.rows[0];
-    return NextResponse.json({ files }, { status: 200 });
+      },
+    )
+    const resData = await res.json()
+    const files = resData.rows[0]
+    return NextResponse.json({ files }, { status: 200 })
   } catch (e) {
-    console.log(e);
+    console.log(e)
     return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    )
   }
 }
