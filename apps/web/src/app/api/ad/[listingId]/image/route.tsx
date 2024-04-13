5102d2b7575e43cee93786989e17e34d88add6f9
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-13 15:16:08 +0200

    remove dynamic

--- apps/web/src/app/api/ad/[listingId]/image/route.tsx
+++ apps/web/src/app/api/ad/[listingId]/image/route.tsx
@@ -3,8 +3,6 @@ import { FrameAspectRatio, adPlaceholderURL } from '@/config/constants'
 import { ImageResponse } from '@vercel/og'
 import { AdLand } from '@/lib/adland'
 
-export const dynamic = 'force-dynamic'
-
 type GetAdsRouteParams = { params: { listingId: string } }
 export async function GET(_req: NextRequest, { params }: GetAdsRouteParams) {
   const { metadata } = await new AdLand().getAdSpace(params.listingId)
