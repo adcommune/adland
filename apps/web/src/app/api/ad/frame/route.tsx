513fd279466a88ecb75d727b4d63ef2b18fe72c0
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-22 16:25:57 +0200

    feat: fc frame: put buy space & ad link behind learn more button. Pinata Analytics

--- apps/web/src/app/api/ad/frame/route.tsx
+++ apps/web/src/app/api/ad/frame/route.tsx
@@ -1,21 +1,33 @@
 import { FrameAspectRatio, baseURL } from '@/config/constants'
 import { AdLand } from '@/lib/adland'
-import { fdk } from '@/lib/pinata'
 import { constants } from '@adland/common'
 import { AdSpace_subgraph } from '@adland/webkit'
 import { FrameButtonMetadata, getFrameHtmlResponse } from '@coinbase/onchainkit'
+
 import { NextRequest, NextResponse } from 'next/server'
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const spaceId = req.nextUrl.searchParams.get('spaceId')!
 
   const frameRequest = await req.json()
+
   try {
     const frame_id = `${constants.chain.name}:frame:${spaceId}`
 
-    await fdk.sendAnalytics(frame_id, frameRequest, spaceId)
+    await fetch('https://api.pinata.cloud/farcaster/frames/interactions', {
+      method: 'POST',
+      body: JSON.stringify({
+        frame_id,
+        data: frameRequest,
+        custom_id: spaceId,
+      }),
+      headers: {
+        Authorization: `Bearer ${process.env.PINATA_JWT}`,
+        'Content-Type': 'application/json',
+      },
+    })
   } catch (error) {
-    console.error('ANALYTICS:error:', error)
+    console.error('PINATA ANALYTICS:', error)
   }
 
   const adSpace = await new AdLand().getAdSpace(spaceId)
