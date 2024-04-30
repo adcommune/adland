814942f53ff9b697c98a63af440cced9738b753b
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-04-30 17:06:11 +0200

    remove hardcoded super tokens

--- apps/web/src/hooks/useAppContracts.tsx
+++ apps/web/src/hooks/useAppContracts.tsx
@@ -11,8 +11,6 @@ const useAppContracts = () => {
   const appChain = constants.chain
 
   return {
-    ethx: superfluidAddresses[appChain.id as AppChainIds].ethx,
-    daix: superfluidAddresses[appChain.id as AppChainIds].daix,
     cfaV1: superfluidAddresses[appChain.id as AppChainIds].cfaV1,
     marketplace: directListingsLogicAddress[appChain.id as AppChainIds],
     adCommonOwnership: commonAdSpacesAddress[appChain.id as AppChainIds],
