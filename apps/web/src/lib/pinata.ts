b001797151dc8b04b84ba99b0afd9cea2beb4cd0
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-05-04 13:08:06 +0200

    move pinata analytics get code inderaction

--- apps/web/src/lib/pinata.ts
+++ apps/web/src/lib/pinata.ts
@@ -1,5 +1,6 @@
 import { constants } from '@adland/common'
 import { FrameRequest } from '@coinbase/onchainkit'
+import { format, subDays } from 'date-fns'
 
 export const postInteraction = async (data: {
   frame_id: string
@@ -14,6 +15,28 @@ export const postInteraction = async (data: {
       'Content-Type': 'application/json',
     },
   }).then((res) => res.json())
+}
+
+export const getInteractions = async (frameId: string) => {
+  // Past 30 days startDate, endDate in the format YYYY-MM-DD HH:MM:SS
+  const startDate = format(subDays(new Date(), 30), 'yyyy-MM-dd HH:mm:ss')
+  const endDate = format(new Date(), 'yyyy-MM-dd HH:mm:ss')
+
+  return fetch(
+    'https://api.pinata.cloud/farcaster/frames/interactions?frame_id=' +
+      frameId +
+      '&start_date=' +
+      startDate +
+      '&end_date=' +
+      endDate,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.PINATA_JWT}`,
+        'Content-Type': 'application/json',
+      },
+    },
+  ).then((res) => res.json())
 }
 
 export const getFramePinataId = (spaceId: string) =>
