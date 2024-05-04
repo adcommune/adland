0e62efb4661363dcac5d197205def2284e837c42
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-05-04 17:44:01 +0200

    try standard hey eth:meta tags

--- apps/web/src/app/group/[id]/[spaceId]/layout.tsx
+++ apps/web/src/app/group/[id]/[spaceId]/layout.tsx
@@ -1,9 +1,36 @@
 import { FrameAspectRatio, baseURL } from '@/config/constants'
+import useAppContracts from '@/hooks/useAppContracts'
+import { constants } from '@adland/common'
 import { FrameMetadata } from '@coinbase/onchainkit'
+import { Address } from 'viem'
 
 type AdSpacePageLayoutProps = {
   children: React.ReactNode
   params: { spaceId: string }
+}
+
+const StandartNFTMetadata = ({
+  chain,
+  collection,
+  contract_address,
+  creator_address,
+  media_url,
+}: {
+  chain: string
+  collection: string
+  contract_address: string | Address
+  media_url: string
+  creator_address?: string | Address
+}) => {
+  return (
+    <>
+      <meta property="eth:nft:chain" content={chain} />
+      <meta property="eth:nft:collection" content={collection} />
+      <meta property="eth:nft:contract_address" content={contract_address} />
+      <meta property="eth:nft:creator_address" content={creator_address} />
+      <meta property="eth:nft:media_url" content={media_url} />
+    </>
+  )
 }
 
 export const dynamic = 'force-dynamic'
@@ -12,6 +39,7 @@ const AdSpacePageLayout = async ({
   children,
   params: { spaceId },
 }: AdSpacePageLayoutProps) => {
+  const { adCommonOwnership } = useAppContracts()
   return (
     <>
       <FrameMetadata
@@ -27,6 +55,14 @@ const AdSpacePageLayout = async ({
           aspectRatio: FrameAspectRatio.SQUARE,
         }}
         postUrl={`${baseURL}/api/ad/frame?spaceId=${spaceId}`}
+      />
+      <StandartNFTMetadata
+        {...{
+          chain: constants.chain.name,
+          collection: 'AdLand: AdSpace #' + spaceId,
+          contract_address: adCommonOwnership,
+          media_url: `${baseURL}/api/billboard/${spaceId}?time=${Date.now()}`,
+        }}
       />
       {children}
     </>
--- apps/web/src/components/FarcasterIntegration.tsx
+++ apps/web/src/components/FarcasterIntegration.tsx
@@ -1,9 +1,26 @@
-import { getFramePinataId, truncateAddress } from '@/lib/utils'
+import { truncateAddress } from '@/lib/utils'
 import { Input } from './ui/input'
 import { baseURL } from '@/config/constants'
 import Copiable from './Copiable'
 import { useQuery } from '@tanstack/react-query'
-import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './ui/table'
+import { Badge } from './ui/badge'
+import { zeroAddress } from 'viem'
+import { getFramePinataId } from '@/lib/pinata'
 
 type FarcasterIntegrationProps = {
   groupId: string
@@ -32,49 +49,96 @@ const FarcasterIntegration = ({
     queryKey: ['farcaster-analytics', spaceId],
   })
 
+  const distributors = [{ account: zeroAddress }]
+
   return (
-    <div className="flex flex-grow flex-col gap-4">
-      <div className="rounded-md border bg-white p-2">
-        <p className="font-body font-semibold">Frame Link</p>
-        <div className="flex w-full flex-row items-center justify-center gap-2">
-          <Input
-            className="h-full flex-grow cursor-default text-opacity-100 disabled:opacity-100"
-            disabled
-            placeholder={truncateAddress(
-              `${baseURL}/group/${groupId}/${spaceId}`,
-              14,
-            )}
-          />
-          <Copiable visible text={`${baseURL}/group/${groupId}/${spaceId}`} />
+    <div className="flex w-full flex-col gap-2">
+      <div className="flex flex-grow flex-col gap-4 rounded-md border bg-white bg-opacity-20 p-4">
+        <div className="rounded-md border bg-white p-2">
+          <p className="font-body font-semibold">Frame Link</p>
+          <div className="flex w-full flex-row items-center justify-center gap-2">
+            <Input
+              className="h-full flex-grow cursor-default text-opacity-100 disabled:opacity-100"
+              disabled
+              placeholder={truncateAddress(
+                `${baseURL}/group/${groupId}/${spaceId}`,
+                14,
+              )}
+            />
+            <Copiable visible text={`${baseURL}/group/${groupId}/${spaceId}`} />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <Card>
+            <CardHeader className="pb-2">
+              <CardDescription>Total interactions</CardDescription>
+              <CardTitle className="text-4xl">
+                {data?.total_interactions ?? 0}
+              </CardTitle>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader className="pb-2">
+              <CardDescription>Unique interactions</CardDescription>
+              <CardTitle className="text-4xl">
+                {data?.total_unique_interactions ?? 0}
+              </CardTitle>
+            </CardHeader>
+          </Card>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4">
-        <Card>
-          <CardHeader className="pb-2">
-            <CardDescription>Total interactions</CardDescription>
-            <CardTitle className="text-4xl">
-              {data?.total_interactions ?? 0}
-            </CardTitle>
-          </CardHeader>
-          {/* <CardContent>
-            <div className="text-xs text-muted-foreground">
-              +10% from last month
-            </div>
-          </CardContent> */}
-        </Card>
-        <Card>
-          <CardHeader className="pb-2">
-            <CardDescription>Unique interactions</CardDescription>
-            <CardTitle className="text-4xl">
-              {data?.total_unique_interactions ?? 0}
-            </CardTitle>
-          </CardHeader>
-          {/* <CardContent>
-            <div className="text-xs text-muted-foreground">
-              +10% from last month
-            </div>
-          </CardContent> */}
-        </Card>
+      <div className="rounded-md border bg-white bg-opacity-20 p-4">
+        <div>
+          <Card>
+            <CardHeader className="px-7">
+              <CardTitle>Distributors</CardTitle>
+              <CardDescription>
+                Accounts distributing this ad space on the farcaster network
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Account</TableHead>
+                    <TableHead className="hidden sm:table-cell">
+                      Followers
+                    </TableHead>
+                    <TableHead className="hidden md:table-cell">
+                      Start
+                    </TableHead>
+                    <TableHead className="text-right">Interactions</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {distributors.map(({ account }) => {
+                    return (
+                      <TableRow className="bg-accent" key={account}>
+                        <TableCell>
+                          <div className="font-medium">Account 1</div>
+                          <div className="hidden text-sm text-muted-foreground md:inline">
+                            {truncateAddress(account)}
+                          </div>
+                        </TableCell>
+                        <TableCell className="hidden sm:table-cell">
+                          Sale
+                        </TableCell>
+                        <TableCell className="hidden sm:table-cell">
+                          <Badge className="text-xs" variant="secondary">
+                            Fulfilled
+                          </Badge>
+                        </TableCell>
+                        <TableCell className="hidden md:table-cell">
+                          2023-06-23
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   )
