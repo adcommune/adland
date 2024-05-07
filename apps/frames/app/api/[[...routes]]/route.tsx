ba1a80e6941d4419a7eeafad6b92403c63d491ae
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-05-07 20:57:01 +0200

    feat: create frames frog application

---
+++ apps/frames/app/api/[[...routes]]/route.tsx
@@ -0,0 +1,83 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog } from "frog";
+import { devtools } from "frog/dev";
+import { neynar } from "frog/hubs";
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
+import { colors, createSystem } from "frog/ui";
+
+export const {
+  Box,
+  Columns,
+  Column,
+  Heading,
+  HStack,
+  Rows,
+  Row,
+  Spacer,
+  Text,
+  VStack,
+  vars,
+} = createSystem({
+  colors: colors.light,
+});
+
+const app = new Frog({
+  assetsPath: "/",
+  basePath: "/api",
+  imageAspectRatio: "1.91:1",
+  ui: { vars },
+  // hub: neynar({ apiKey: process.env.NEYNAR_API_KEY ?? "" }),
+});
+
+// Uncomment to use Edge Runtime
+export const runtime = "edge";
+
+const fruitColors: Record<string, keyof typeof colors.light> = {
+  apple: "green",
+  banana: "amber300",
+  cherry: "red",
+  date: "amber1000",
+};
+
+app.frame("/", (c) => {
+  const { buttonValue, inputText, status } = c;
+  const fruit = inputText || buttonValue;
+
+  return c.res({
+    title: "Fruit Picker",
+    image: (
+      <Box grow>
+        <HStack gap="8" grow>
+          <Column grow alignVertical="center" alignHorizontal="center">
+            Fruit
+          </Column>
+          {buttonValue ? (
+            <Column
+              grow
+              alignVertical="center"
+              alignHorizontal="center"
+              backgroundColor={fruitColors[buttonValue]}
+            >
+              {fruit ?? "no-fruit"}
+            </Column>
+          ) : (
+            <></>
+          )}
+        </HStack>
+      </Box>
+    ),
+    intents: [
+      <Button value="apple">Apple</Button>,
+      <Button value="banana">Banana</Button>,
+      <Button value="cherry">Cherry</Button>,
+      <Button value="date">Date</Button>,
+    ],
+  });
+});
+
+devtools(app, { serveStatic, appFid: 1733 });
+
+export const GET = handle(app);
+export const POST = handle(app);
