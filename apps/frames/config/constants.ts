b6f17506c0af6cb5b7fd693535401c1d4042f508
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-05-08 09:44:05 +0200

    setup aidrop config for frame

--- apps/frames/config/constants.ts
+++ apps/frames/config/constants.ts
@@ -1,7 +1,19 @@
 import { Address } from "viem";
 import { optimismSepolia, base } from "viem/chains";
 
-export const airdropToken: Record<number, Address> = {
-  [optimismSepolia.id]: "0x4247ba6c3658fa5c0f523bacea8d0b97af1a175e", // fDAI
-  [base.id]: "0x4ed4e862860bed51a9570b96d89af5e1b0efefed", // Degen
+export const airdropConfig: Record<
+  number,
+  {
+    token: Address;
+    amount: bigint;
+  }
+> = {
+  [optimismSepolia.id]: {
+    token: "0x4247ba6c3658fa5c0f523bacea8d0b97af1a175e",
+    amount: BigInt(1),
+  }, // fDAI
+  [base.id]: {
+    token: "0x4ed4e862860bed51a9570b96d89af5e1b0efefed",
+    amount: BigInt(1),
+  },
 };
