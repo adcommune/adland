554551cb16adf338ce26430d153b421f425e07cd
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-05-28 22:53:06 +0200

    review groups page

--- apps/web/src/app/group/[id]/page.tsx
+++ apps/web/src/app/group/[id]/page.tsx
@@ -4,13 +4,17 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { AdLand } from '@/lib/adland'
 import { Separator } from '@/components/ui/separator'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import AdGroupActivity from '@/components/AdGroup/AdGroupActivity'
+import { AdGroup, AdSpace } from '@/lib/types'
 
 type GroupPageProps = { params: { id: string } }
 
 const GroupPage = async ({ params: { id } }: GroupPageProps) => {
-  const adGroup = await new AdLand().getGroup(id)
+  const adGroup = (await new AdLand().getGroup(id)) as Omit<
+    AdGroup,
+    'adSpaces'
+  > & {
+    adSpaces: AdSpace[]
+  }
 
   if (!adGroup) {
     return (
@@ -21,62 +25,63 @@ const GroupPage = async ({ params: { id } }: GroupPageProps) => {
   }
 
   const { adSpaces } = adGroup
-
   return (
-    <Tabs defaultValue="ad-spaces" className="w-full">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="ad-spaces">Ad Spaces</TabsTrigger>
-        <TabsTrigger value="activity">Activity</TabsTrigger>
-      </TabsList>
-      <TabsContent value="ad-spaces" className="w-full">
-        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
-          {adSpaces?.map(({ adSpace_subgraph: adSpace, metadata }, index) => {
-            return (
-              <Link
-                key={adSpace?.transactionHash + adSpace?.id}
-                href={'/ad/' + adSpace?.id}
-              >
-                <Card className="flex flex-col overflow-hidden">
-                  <CardHeader>
-                    <CardTitle className="font-body font-normal text-gray-500">
-                      AdSpace #{index}
-                    </CardTitle>
-                  </CardHeader>
-                  <CardContent className="relative flex h-[400px] w-full flex-grow flex-col gap-2 bg-gray-100 py-4">
-                    {metadata?.imageGatewayURI && (
-                      <div className="flex h-2/3 flex-grow bg-gray-200">
-                        <Image
-                          width={500}
-                          height={500}
-                          alt="AdSpace Image"
-                          className=" w-full object-contain"
-                          src={metadata?.imageGatewayURI}
-                        />
-                      </div>
-                    )}
-                    <Separator />
-                    {metadata?.description && (
-                      <p className="text-left font-body text-sm font-semibold">
-                        {metadata?.description}
-                      </p>
-                    )}
-                    <Separator />
-                    {metadata?.external_url && (
-                      <p className="text-left text-sm font-light">
-                        {metadata?.external_url}
-                      </p>
-                    )}
-                  </CardContent>
-                </Card>
-              </Link>
-            )
-          })}
-        </div>
-      </TabsContent>
-      <TabsContent value="activity" className="w-full">
-        <AdGroupActivity adGroup={adGroup} />
-      </TabsContent>
-    </Tabs>
+    <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
+      {adSpaces?.map(({ adSpace_subgraph: adSpace, metadata }, index) => {
+        const liveDistribution = adSpace.adPools.length > 0
+
+        return (
+          <Link
+            key={adSpace?.transactionHash + adSpace?.id}
+            href={'/ad/' + adSpace?.id}
+          >
+            <Card className="flex flex-col overflow-hidden bg-transparent">
+              <CardHeader className="relative bg-white">
+                <CardTitle className="flex w-full flex-row items-center justify-between font-body font-normal">
+                  <span>AdSpace #{adSpace.id}</span>
+                  {liveDistribution && (
+                    <span className="relative flex h-3 w-3">
+                      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                      <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
+                    </span>
+                  )}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="relative flex h-[400px] w-full flex-grow flex-col gap-2 bg-white bg-opacity-75 py-4">
+                {!metadata && (
+                  <div className="flex h-full w-full flex-col items-center justify-center">
+                    <p className="font-display text-2xl">No Ad</p>
+                  </div>
+                )}
+                {metadata?.imageGatewayURI && (
+                  <div className="flex h-2/3 flex-grow bg-gray-200">
+                    <Image
+                      width={500}
+                      height={500}
+                      alt="AdSpace Image"
+                      className=" w-full object-contain"
+                      src={metadata?.imageGatewayURI}
+                    />
+                  </div>
+                )}
+                {metadata && <Separator />}
+                {metadata?.description && (
+                  <p className="text-left font-body text-sm font-semibold">
+                    {metadata?.description}
+                  </p>
+                )}
+                {metadata && <Separator />}
+                {metadata?.external_url && (
+                  <p className="text-left text-sm font-light">
+                    {metadata?.external_url}
+                  </p>
+                )}
+              </CardContent>
+            </Card>
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
