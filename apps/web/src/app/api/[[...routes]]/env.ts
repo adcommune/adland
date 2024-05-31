74b7fdb3f4c52495783c6a002025023a435c0f45
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-05-31 16:19:28 +0200

    add pool claim env flag

--- apps/web/src/app/api/[[...routes]]/env.ts
+++ apps/web/src/app/api/[[...routes]]/env.ts
@@ -4,8 +4,11 @@ import { get } from '@vercel/edge-config'
 const env = constants.chain.testnet ? 'testnet' : 'mainnet'
 
 export const distributionEnabled = async () =>
-  (await get(`${env}-distribution`)) === 'true' ||
-  process.env.NODE_ENV === 'development'
+  (await get(`${env}-distribution`)) === 'true'
 
 export const shouldRecastDistributor = async () =>
   (await get(`${env}-distribution-recast`)) === 'true'
+
+export const poolClaimEnabled = async () =>
+  (await get(`${env}-pool-claim`)) === 'true' ||
+  process.env.NODE_ENV === 'development'
