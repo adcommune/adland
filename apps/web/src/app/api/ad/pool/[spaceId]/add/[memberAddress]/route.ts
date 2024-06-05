9ede229bbe4a2944c49f473d211758d8857d7b64
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-06-05 19:28:09 +0200

    feat: add superfluid data fetching methods

--- apps/web/src/app/api/ad/pool/[spaceId]/add/[memberAddress]/route.ts
+++ apps/web/src/app/api/ad/pool/[spaceId]/add/[memberAddress]/route.ts
@@ -1,5 +1,7 @@
 import { CommonAdPool } from '@/lib/adPool'
 import { AdLand } from '@/lib/adland'
+import { neynar } from '@/lib/neynar'
+import { publicClient } from '@/lib/viem'
 import { NextRequest, NextResponse } from 'next/server'
 import { Address } from 'viem'
 
@@ -8,6 +10,30 @@ export const POST = async (
   { params }: { params: { spaceId: string; memberAddress: Address } },
 ) => {
   const { spaceId, memberAddress } = params
+  const { message, signature } = (await _req.json()) as {
+    message: string
+    signature: `0x${string}`
+  }
+
+  const valid = await publicClient.verifyMessage({
+    message,
+    signature,
+    address: memberAddress,
+  })
+
+  if (!valid) {
+    return NextResponse.json({ error: 'Invalid signature' })
+  }
+
+  const data = message.split('-')
+  const castHash = data[0]
+  const fid = parseInt(data[1])
+
+  const cast = (await neynar.fetchBulkCasts([castHash])).result.casts[0]
+
+  if (!cast) {
+    return NextResponse.json({ error: 'Invalid cast hash' })
+  }
 
   const adSpace = await new AdLand().getAdSpace(spaceId, {
     withMetadata: false,
@@ -33,7 +59,7 @@ export const POST = async (
 
   const pool = new CommonAdPool(poolAddress)
 
-  const receipt = await pool.addMemeberToGroup(memberAddress, 1000)
+  const receipt = await pool.updateMemberUnits(memberAddress, 1000)
 
   if (receipt.status !== 'success') {
     return NextResponse.json({
