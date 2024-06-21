b349bb8620d6d754b23bcb85dd4d741fcf057fcf
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-06-22 00:56:16 +0200

    feat: put:wq :@sh ponder mainnet settings

--- packages/adland-common/src/constants.mainnet.ts
+++ packages/adland-common/src/constants.mainnet.ts
@@ -2,7 +2,7 @@ import { base } from "viem/chains";
 import { Constants } from "./types";
 
 export const constants: Constants = {
-  ponderUrl: "",
+  ponderUrl: "https://adland-base.up.railway.app",
   superfluidSubgraphUrl: "https://base-mainnet.subgraph.x.superfluid.dev/",
   subgraphUrl:
     "https://api.studio.thegraph.com/query/958/adland-base/version/latest",
--- packages/adland-ponder/ponder.config.ts
+++ packages/adland-ponder/ponder.config.ts
@@ -4,25 +4,51 @@ import { http } from "viem";
 import { CommonAdSpacesAbi } from "./abis/CommonAdSpacesAbi";
 import { DirectListingAbi } from "./abis/DirectListingAbi";
 
-export default createConfig({
-  networks: {
-    "optimism-sepolia": {
-      chainId: 11155420,
-      transport: http(process.env.PONDER_RPC_URL_11155420),
+let config;
+
+if (process.env.STAGE === "mainnet") {
+  config = createConfig({
+    networks: {
+      base: { chainId: 8453, transport: http(process.env.PONDER_RPC_URL_8453) },
     },
-  },
-  contracts: {
-    CommonAdSpaces: {
-      network: "optimism-sepolia",
-      address: "0xB30D0dcF2A4A702dB746375FCac01C4F0e5d0Bd1",
-      abi: CommonAdSpacesAbi,
-      startBlock: 13331750,
+    contracts: {
+      CommonAdSpaces: {
+        network: "base",
+        address: "0x56324569b301CdA15AFb3F9E9A853b6a5cD1ca57",
+        abi: CommonAdSpacesAbi,
+        startBlock: 15741119,
+      },
+      DirectListing: {
+        network: "base",
+        address: "0x9825f700754534108BFE2239C9e66a12FDEBB33e",
+        abi: DirectListingAbi,
+        startBlock: 15741119,
+      },
     },
-    DirectListing: {
-      network: "optimism-sepolia",
-      address: "0x44f808B028cD582b21C04f6de3580029d3E31Cb6",
-      abi: DirectListingAbi,
-      startBlock: 13331750,
+  });
+} else {
+  config = createConfig({
+    networks: {
+      "optimism-sepolia": {
+        chainId: 11155420,
+        transport: http(process.env.PONDER_RPC_URL_11155420),
+      },
     },
-  },
-});
+    contracts: {
+      CommonAdSpaces: {
+        network: "optimism-sepolia",
+        address: "0xB30D0dcF2A4A702dB746375FCac01C4F0e5d0Bd1",
+        abi: CommonAdSpacesAbi,
+        startBlock: 13331750,
+      },
+      DirectListing: {
+        network: "optimism-sepolia",
+        address: "0x44f808B028cD582b21C04f6de3580029d3E31Cb6",
+        abi: DirectListingAbi,
+        startBlock: 13331750,
+      },
+    },
+  });
+}
+
+export default config;
