115178291ccb6ecd440dd0dcb49cf368e21e5fd5
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-06-23 11:54:16 +0200

    remove dead code

--- apps/web/src/components/ConnectButton.tsx
+++ apps/web/src/components/ConnectButton.tsx
@@ -5,13 +5,9 @@ import { zeroAddress } from 'viem'
 import Link from 'next/link'
 import { UserIcon } from 'lucide-react'
 import FarcasterBadge from './FarcasterBadge'
-import { useContext } from 'react'
-import { SmartAccountContext } from '@/context/SmartAccountContext'
 
 export const ConnectButton = () => {
   const { ready, authenticated, user, login } = usePrivy()
-
-  const { balance } = useContext(SmartAccountContext)
 
   if (!ready)
     return (
@@ -31,11 +27,11 @@ export const ConnectButton = () => {
   return (
     <Link href="/profile">
       {user?.farcaster ? (
-        <div className="flex h-10 flex-row gap-2 font-body">
+        <div className="flex flex-row gap-2 font-body">
           <FarcasterBadge className="h-11" />
         </div>
       ) : (
-        <div className="flex h-10 flex-row gap-2">
+        <div className="flex flex-row gap-2">
           <Button type="button" variant="outline" className="h-full font-body">
             <UserIcon className="h-4 w-4" />
           </Button>
