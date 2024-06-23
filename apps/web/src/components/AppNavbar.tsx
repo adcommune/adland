099d425f7db7319481306f50bcb41c95b5884ef3
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-06-23 11:55:12 +0200

    place create Ads on different places in mobile & desktop

--- apps/web/src/components/AppNavbar.tsx
+++ apps/web/src/components/AppNavbar.tsx
@@ -1,12 +1,26 @@
 'use client'
 
+import Link from 'next/link'
 import { ConnectButton } from './ConnectButton'
 import Navbar from './Navbar'
+import { Button } from './ui/button'
+import { PlusIcon } from 'lucide-react'
+import { useContext } from 'react'
+import { SmartAccountContext } from '@/context/SmartAccountContext'
 
 const AppNavbar = () => {
+  const { bicoAccountAddress } = useContext(SmartAccountContext)
   return (
     <Navbar>
       <div className="flex flex-row gap-2">
+        {bicoAccountAddress && (
+          <Link className="hidden sm:flex" href={'/group/create'}>
+            <Button className="h-full w-full gap-2">
+              <PlusIcon size={16} />
+              Create Ads
+            </Button>
+          </Link>
+        )}
         <ConnectButton />
       </div>
     </Navbar>
