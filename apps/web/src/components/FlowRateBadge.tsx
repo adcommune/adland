061c0d30b249934a102ae9c60f537b02ded89d7d
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-07-03 15:57:39 +0200

    flow rate badge component

---
+++ apps/web/src/components/FlowRateBadge.tsx
@@ -0,0 +1,30 @@
+import { formatAmount } from '@/lib/helpers'
+import { Badge } from './ui/badge'
+import TokenImage from './TokenImage'
+import { formatEther } from 'viem'
+
+const FlowRateBadge = ({
+  flowRate,
+  currency,
+}: {
+  flowRate: bigint
+  currency: string
+}) => {
+  return (
+    <Badge className="flex flex-row gap-2 border">
+      <span className="relative flex h-3 w-3">
+        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+        <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
+      </span>
+      <>
+        <div className="flex flex-row gap-1">
+          {formatAmount(formatEther(flowRate))}
+          <TokenImage address={currency} className="h-4 w-4" />
+        </div>
+        /week
+      </>
+    </Badge>
+  )
+}
+
+export default FlowRateBadge
