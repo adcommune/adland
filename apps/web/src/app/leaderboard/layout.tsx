d3319a80e1b382b990897593481aaf3b3d660367
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-07-04 21:23:30 +0200

    fix: center row

--- apps/web/src/app/leaderboard/layout.tsx
+++ apps/web/src/app/leaderboard/layout.tsx
@@ -9,7 +9,7 @@ const LeaderboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container>
       <Card className="mt-5 font-body">
-        <CardHeader className="flex flex-row justify-between">
+        <CardHeader className="flex flex-row items-center justify-between">
           <CardTitle className="font-display">Leaderboard</CardTitle>
           <Link href="/leaderboard/how-does-it-work">
             <Button variant="outline" size="sm" className="flex flex-row gap-2">
